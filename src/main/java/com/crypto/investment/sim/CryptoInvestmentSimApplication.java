ea6bee0504516958a590c0ad3f8167d54247d65e
CameronWard301 <38313327+CameronWard301@users.noreply.github.com>
2021-11-23 21:58:46 +0000

    Created Coin Objects and Store in DB

--- src/main/java/com/crypto/investment/sim/CryptoInvestmentSimApplication.java
+++ src/main/java/com/crypto/investment/sim/CryptoInvestmentSimApplication.java
@@ -1,13 +1,42 @@
 package com.crypto.investment.sim;
 
+import com.crypto.investment.sim.model.Coin;
+import com.crypto.investment.sim.repos.CoinRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class CryptoInvestmentSimApplication {
+@EnableScheduling
+public class CryptoInvestmentSimApplication implements ApplicationRunner {
 
-    public static void main(String[] args) {
+    public static Coin BTC = new Coin("BTC", "Bitcoin");
+    public static Coin ETH = new Coin("ETC", "Ethereum");
+    public static Coin ADA = new Coin("ADA", "Cardano");
+    public static Coin GBP = new Coin("GBP", "Pounds", 1);
+    public static Coin EUR = new Coin("EUR", "Euros");
+    public static Coin USD = new Coin("USD", "Dollars");
+
+    @SuppressWarnings("SpringJavaAutowiredFieldsWarningInspection")
+    @Autowired
+    public CoinRepository coinRepo;
+
+
+    public static void main(String[] args){
+
         SpringApplication.run(CryptoInvestmentSimApplication.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) {
+        BTC = coinRepo.save(BTC);
+        ETH = coinRepo.save(ETH);
+        ADA = coinRepo.save(ADA);
+        GBP = coinRepo.save(GBP);
+        EUR = coinRepo.save(EUR);
+        USD = coinRepo.save(USD);
+    }
 }
