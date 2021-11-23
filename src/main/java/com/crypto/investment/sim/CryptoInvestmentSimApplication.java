2420428a63f31a2933349d89702e9ac0daba7368
Cameron <cameronward007@gmail.com>
2021-11-23 12:54:09 +0000

    Project Init

---
+++ src/main/java/com/crypto/investment/sim/CryptoInvestmentSimApplication.java
@@ -0,0 +1,13 @@
+package com.crypto.investment.sim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoInvestmentSimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoInvestmentSimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crypto/investment/sim/CryptoInvestmentSimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crypto.investment.sim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoInvestmentSimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
