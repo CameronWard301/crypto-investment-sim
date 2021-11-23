7673e55cd163eff741726a100fb0e80e3fa38b0e
joshcampbell7 <joshuacampbell49@yahoo.co.uk>
2021-11-23 17:36:33 +0000

    Added coin class

---
+++ src/main/java/com/crypto/investment/sim/model/Coin.java
@@ -0,0 +1,40 @@
+package com.crypto.investment.sim.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Coin {
+    @Id
+    private int CoinID;
+    private String CoinCode;
+    private String Name;
+    private float CurrentPrice;
+
+    public int getCoinID() {
+        return CoinID;
+    }
+    public void setCoinID(int coinID) {
+        CoinID = coinID;
+    }
+    public String getName() {
+        return Name;
+    }
+    public void setName(String name) {
+        Name = name;
+    }
+    public float getCurrentPrice() {
+        return CurrentPrice;
+    }
+    public void setCurrentPrice(float currentPrice) {
+        CurrentPrice = currentPrice;
+    }
+
+    public String getCoinCode() {
+        return CoinCode;
+    }
+
+    public void setCoinCode(String coinCode) {
+        CoinCode = coinCode;
+    }
+}
