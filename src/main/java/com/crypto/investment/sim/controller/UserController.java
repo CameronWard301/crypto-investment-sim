ea9bf9a5bc7c17860ddab7890faa6c5ee59d143e
joshu <jrw40@student.le.ac.uk>
2021-11-26 00:16:33 +0000

    get coins from the database and add them as attributes to the model for use in /portfolio.jsp

--- src/main/java/com/crypto/investment/sim/controller/UserController.java
+++ src/main/java/com/crypto/investment/sim/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.crypto.investment.sim.controller;
 
+import com.crypto.investment.sim.model.Coin;
 import com.crypto.investment.sim.model.User;
+import com.crypto.investment.sim.repos.CoinRepository;
 import com.crypto.investment.sim.repos.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -15,10 +19,23 @@ import java.util.Optional;
 public class UserController {
     @Autowired
     public UserRepository userRepo;
+    @Autowired
+    public CoinRepository coinRepo;
+
     @GetMapping("/portfolio")
     public String viewPortfolio(@RequestParam("user") int id, Model model) {
         Optional<User> user = userRepo.findById(id);
         user.ifPresent(value -> model.addAttribute("user", value));
+
+        Optional<Coin> btc = coinRepo.findById("BTC");
+        btc.ifPresent(value -> model.addAttribute("btc", value));
+
+        Optional<Coin> eth = coinRepo.findById("ETH");
+        eth.ifPresent(value -> model.addAttribute("eth", value));
+
+        Optional<Coin> ada = coinRepo.findById("ADA");
+        ada.ifPresent(value -> model.addAttribute("ada", value));
+
         return "user/portfolio";
     }
 }
