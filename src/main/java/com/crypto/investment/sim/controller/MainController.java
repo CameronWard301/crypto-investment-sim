cb24d2ac5f09885614e5e10c9356e74592b4e728
Cameron <cameronward007@gmail.com>
2021-12-03 19:43:55 +0000

    Added controller for markets page

--- src/main/java/com/crypto/investment/sim/controller/MainController.java
+++ src/main/java/com/crypto/investment/sim/controller/MainController.java
@@ -1,13 +1,35 @@
 package com.crypto.investment.sim.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class MainController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String getIndex() {
         return "start";
+    }
+
+    @GetMapping("/markets")
+    public String getMarkets(@RequestParam(value="fiat", required = false) Optional<Integer> id, Model model) {
+        if (id.isEmpty()){
+            //If no parameter show default markets in £££
+            model.addAttribute("fiat", 1508);
+        } else if (id.get() == 1505){
+            //Show markets in $$$
+            model.addAttribute("fiat", 1505);
+        } else if (id.get() == 1506){
+            //Show markets in €€€
+            model.addAttribute("fiat", 1506);
+        } else{
+            //If a different param value is passed fall back on £££
+            model.addAttribute("fiat", 1508);
+        }
+        return "markets";
     }
 }
