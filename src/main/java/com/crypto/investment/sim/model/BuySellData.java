7882d4f36f2c04993391434f2985e1e754ec201c
Cameron <cameronward007@gmail.com>
2021-12-28 15:29:50 +0000

    converted objects to strings from floats added a function to calculate the currency conversion

--- src/main/java/com/crypto/investment/sim/model/BuySellData.java
+++ src/main/java/com/crypto/investment/sim/model/BuySellData.java
@@ -1,11 +1,15 @@
 package com.crypto.investment.sim.model;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Objects;
+
 @SuppressWarnings("unused")
 public class BuySellData {
     private String convertFrom;
     private String convertTo;
-    private float convertFromAmount;
-    private float estimatedExchange;
+    private String convertFromAmount;
+    private String estimatedExchange;
 
     public String getConvertFrom() {
         return convertFrom;
@@ -23,19 +27,38 @@ public class BuySellData {
         this.convertTo = convertTo;
     }
 
-    public float getConvertFromAmount() {
+    public String  getConvertFromAmount() {
         return convertFromAmount;
     }
 
-    public void setConvertFromAmount(float convertFromAmount) {
+    public void setConvertFromAmount(String convertFromAmount) {
         this.convertFromAmount = convertFromAmount;
     }
 
-    public float getEstimatedExchange() {
+    public String getEstimatedExchange() {
         return estimatedExchange;
     }
 
-    public void setEstimatedExchange(float estimatedExchange) {
+    public void setEstimatedExchange(String estimatedExchange) {
         this.estimatedExchange = estimatedExchange;
+    }
+
+    public float calculateExchange(String fromCurrency, String toCurrency, float fromAmount, float fromCoinValue, float toCoinValue){
+        float calculatedAmount;
+        if (Objects.equals(fromCurrency, "GBP") || Objects.equals(fromCurrency, "USD") || Objects.equals(fromCurrency, "EUR")){
+            calculatedAmount = fromAmount*(toCoinValue/fromCoinValue);
+        } else {
+            calculatedAmount = (fromAmount/fromCoinValue)*toCoinValue;
+        }
+        if (Objects.equals(toCurrency, "GBP") || Objects.equals(toCurrency, "USD") || Objects.equals(toCurrency, "EUR")){
+            return this.roundDown(calculatedAmount);
+        }
+        return calculatedAmount;
+    }
+
+    public float roundDown(float amount){
+        BigDecimal bd = new BigDecimal(Float.toString(amount));
+        bd = bd.setScale(2, RoundingMode.HALF_DOWN);
+        return bd.floatValue();
     }
 }
