ed33392c6c7e97cfe7a11a869efe75ca6e74716a
CameronWard301 <38313327+CameronWard301@users.noreply.github.com>
2021-12-30 16:36:29 +0000

    Exchange coin on the user's database object if there are no errors

--- src/main/java/com/crypto/investment/sim/controller/BuySellController.java
+++ src/main/java/com/crypto/investment/sim/controller/BuySellController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Optional;
 
 @Controller
@@ -32,7 +34,7 @@ public class BuySellController {
     public UserRepository userRepo;
 
     @InitBinder
-    protected void initBinder(WebDataBinder binder, HttpSession session){
+    protected void initBinder(WebDataBinder binder, HttpSession session) {
         binder.addValidators(new ExchangeValidator(userRepo, coinRepo, session));
     }
 
@@ -42,6 +44,12 @@ public class BuySellController {
         if (USER_SESSION == null) {
             session.setAttribute("message", "Please login and try again");
             return "redirect:/login";
+        }
+        if (session.getAttribute("message") != null) {
+            model.addAttribute("bannerColor", session.getAttribute("banner-color"));
+            model.addAttribute("message", session.getAttribute("message"));
+            session.removeAttribute("message");
+            model.addAttribute("hidden", "show");
         }
         model.addAttribute("buySellData", new BuySellData());
         model.addAttribute("user", USER_SESSION);
@@ -53,21 +61,64 @@ public class BuySellController {
     @PostMapping("/convertRequest")
     public String finalTransaction(@Valid @ModelAttribute BuySellData buySellData, BindingResult result, Model model, HttpSession session) {
         User USER_SESSION = (User) session.getAttribute("USER_SESSION");
-        if (result.hasErrors()){
+        if (USER_SESSION == null) {
+            session.setAttribute("message", "You have automatically been logged out. Please login and try again");
+            return "redirect:/login";
+        }
+        if (result.hasErrors()) {
             model.addAttribute("user", USER_SESSION);
+            model.addAttribute("bannerColor", "banner-color-red");
+            model.addAttribute("message", "Please review the errors below and try again");
+            session.removeAttribute("message");
+            model.addAttribute("hidden", "show");
             this.getLatestCoins(model);
             return "user/buySell";
         }
 
         Optional<Coin> fromCoinObject = coinRepo.findById(buySellData.getConvertFrom());
         Optional<Coin> toCoinObject = coinRepo.findById(buySellData.getConvertTo());
+        Optional<User> dbUser = userRepo.findById(USER_SESSION.getId());
 
-        if (fromCoinObject.isEmpty() || toCoinObject.isEmpty()) {
+        if (fromCoinObject.isEmpty() || toCoinObject.isEmpty() || dbUser.isEmpty()) {
+            session.setAttribute("message", "Could not resolve coin objects, please log out and try again");
+            session.setAttribute("banner-color", "banner-color-red");
             return "redirect:/buySell";
         }
 
         float convertAmount = Float.parseFloat(buySellData.getConvertFromAmount());
         float exchangedCoin = buySellData.calculateExchange(buySellData.getConvertFrom(), buySellData.getConvertTo(), convertAmount, fromCoinObject.get().getCurrentPrice(), toCoinObject.get().getCurrentPrice());
+
+        Method setFromCoin;
+        Method setToCoin;
+        Method getFromCoin;
+        Method getToCoin;
+
+        try {
+            setFromCoin = User.class.getDeclaredMethod("set" + buySellData.getConvertFrom(), float.class);
+            setToCoin = User.class.getDeclaredMethod("set" + buySellData.getConvertTo(), float.class);
+            getFromCoin = User.class.getDeclaredMethod("get" + buySellData.getConvertFrom());
+            getToCoin = User.class.getDeclaredMethod("get" + buySellData.getConvertTo());
+            float currentFromCoinBalance = (float) getFromCoin.invoke(dbUser.get());
+            float currentToCoinBalance = (float) getToCoin.invoke(dbUser.get());
+            setFromCoin.invoke(dbUser.get(), currentFromCoinBalance - convertAmount);
+            setToCoin.invoke(dbUser.get(), currentToCoinBalance + exchangedCoin);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            session.setAttribute("message", "Could not update user, please log out and try again");
+            session.setAttribute("banner-color", "banner-color-red");
+            return "redirect:/buySell";
+        }
+
+        User updatedUser = userRepo.save(dbUser.get());
+        session.setAttribute("USER_SESSION", updatedUser);
+
+        if (Float.parseFloat(buySellData.getEstimatedExchange()) != exchangedCoin) {
+            session.setAttribute("message", "Success! You have traded currencies.\nPlease note the markets updated during the transaction/viewing this page\nInstead of receiving " +
+                    buySellData.getEstimatedExchange() + " " + buySellData.getConvertTo() + " You actually received: " + exchangedCoin + " " + buySellData.getConvertTo());
+        } else {
+            session.setAttribute("message", "Success! You have traded currencies");
+        }
+        session.setAttribute("banner-color", "banner-color-green");
+
 
         return "redirect:/buySell";
     }
