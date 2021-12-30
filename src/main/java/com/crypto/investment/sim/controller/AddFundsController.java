524f3afce9e88a394ead508ed90f77cc49d9fbff
CameronWard301 <38313327+CameronWard301@users.noreply.github.com>
2021-12-30 16:38:12 +0000

    added custom messages and footer

--- src/main/java/com/crypto/investment/sim/controller/AddFundsController.java
+++ src/main/java/com/crypto/investment/sim/controller/AddFundsController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Objects;
 
 @Controller
@@ -36,38 +38,47 @@ public class AddFundsController {
     public String addRemove(@Valid @ModelAttribute UserAddFunds userAddFunds, BindingResult result, Model model, HttpSession session) {
         User USER_SESSION = (User) session.getAttribute("USER_SESSION");
         if (USER_SESSION == null) {
-            session.setAttribute("message", "Please login and try again");
+            session.setAttribute("message", "You have automatically been logged out. Please login and try again");
             return "redirect:/login";
         }
         this.addAttributes(model, USER_SESSION);
         model.addAttribute("selected", userAddFunds.getFiat());
         if (result.hasErrors()) {
+            model.addAttribute("message", "Please correct the errors below and try again");
+            model.addAttribute("bannerColor", "banner-color-red");
+            model.addAttribute("hidden", "show");
             return "user/addRemoveCurrency";
         }
 
+        String symbol;
         if (Objects.equals(userAddFunds.getFiat(), "USD")) {
             //$$$
-            USER_SESSION.setUSD(USER_SESSION.getUSD() + Float.parseFloat(userAddFunds.getValue())); //update the session object
+            USER_SESSION.setUSD(USER_SESSION.getUSD() + roundDown(Float.parseFloat(userAddFunds.getValue()))); //update the session object
             model.addAttribute("selected", "USD"); //select the dollar button on page
-            model.addAttribute("USD", USER_SESSION.getUSD()); //add the new value of USD to the page
+            model.addAttribute("USD", roundFloat(USER_SESSION.getUSD())); //add the new value of USD to the page
             userRepo.save(USER_SESSION); //save the user in the database
             session.setAttribute("USER_SESSION", USER_SESSION); //overwrite session
+            symbol = "$";
         } else if (Objects.equals(userAddFunds.getFiat(), "EUR")) {
             //€€€
-            USER_SESSION.setEUR(USER_SESSION.getEUR() + Float.parseFloat(userAddFunds.getValue()));
+            USER_SESSION.setEUR(USER_SESSION.getEUR() + roundDown(Float.parseFloat(userAddFunds.getValue())));
             model.addAttribute("selected", "EUR");
-            model.addAttribute("EUR", USER_SESSION.getEUR());
+            model.addAttribute("EUR", roundFloat(USER_SESSION.getEUR()));
             userRepo.save(USER_SESSION);
             session.setAttribute("USER_SESSION", USER_SESSION);
+            symbol = "€";
         } else {
             //£££
-            USER_SESSION.setGBP(USER_SESSION.getGBP() + Float.parseFloat(userAddFunds.getValue()));
+            USER_SESSION.setGBP(USER_SESSION.getGBP() + roundDown(Float.parseFloat(userAddFunds.getValue())));
             model.addAttribute("selected", "GBP");
-            model.addAttribute("GBP", USER_SESSION.getGBP());
+            model.addAttribute("GBP", roundFloat(USER_SESSION.getGBP()));
             userRepo.save(USER_SESSION);
             session.setAttribute("USER_SESSION", USER_SESSION);
+            symbol = "£";
         }
-        return "user/addRemoveCurrency";
+        session.setAttribute("message", "Success! You have amended your account by: "+symbol + roundFloat(roundDown(Float.parseFloat(userAddFunds.getValue()))));
+        session.setAttribute("banner-color", "banner-color-green");
+        return "redirect:/addRemoveCurrency";
     }
 
     @GetMapping("/addRemoveCurrency")
@@ -77,6 +88,12 @@ public class AddFundsController {
             session.setAttribute("message", "Please login and try again");
             return "redirect:/login";
         }
+        if (session.getAttribute("message") != null){
+            model.addAttribute("bannerColor", session.getAttribute("banner-color"));
+            model.addAttribute("message", session.getAttribute("message"));
+            model.addAttribute("hidden", "show");
+            session.removeAttribute("message");
+        }
         this.addAttributes(model, USER_SESSION);
         model.addAttribute("userAddFunds", new UserAddFunds());
         model.addAttribute("selected", "GBP");
@@ -85,9 +102,19 @@ public class AddFundsController {
     }
 
     private void addAttributes(Model model, User USER_SESSION) {
-        model.addAttribute("GBP", USER_SESSION.getGBP());
-        model.addAttribute("USD", USER_SESSION.getUSD());
-        model.addAttribute("EUR", USER_SESSION.getEUR());
+        model.addAttribute("GBP", roundFloat(USER_SESSION.getGBP()));
+        model.addAttribute("USD", roundFloat(USER_SESSION.getUSD()));
+        model.addAttribute("EUR", roundFloat(USER_SESSION.getEUR()));
 
+    }
+
+    public float roundDown(float amount){
+        BigDecimal bd = new BigDecimal(Float.toString(amount));
+        bd = bd.setScale(2, RoundingMode.HALF_DOWN);
+        return bd.floatValue();
+    }
+
+    private String roundFloat(float f){
+        return String.format("%.2f", f);
     }
 }
